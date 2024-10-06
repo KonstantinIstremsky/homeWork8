06202298929d0aabfd4556af0de0942d983a4f5a
KonstantinIstremsky <kalter.storyteller@gmail.com>
2024-10-06 13:57:12 +0300

    HW-8

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
+public void main(String[] args) {
+    // task 1
+    System.out.println("Task 1 and Task 2");
+    byte[] arr1 = new byte[3];
+    arr1[0] = 1;
+    arr1[1] = 2;
+    arr1[2] = 3;
+    System.out.println(Arrays.toString(arr1));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    float[] arr2 = {1.57f, 7.654f, 9.986f};
+    System.out.println(Arrays.toString(arr2));
+
+    byte[] arr3 = {1, 7, 9};
+    System.out.println(Arrays.toString(arr3));
+
+    // task 3
+    System.out.println("Task 3");
+
+    StringBuilder result1 = new StringBuilder();
+    for (int i = arr1.length - 1; i >= 0; i--) {
+        result1.append(arr1[i]);
+        if (i != 0) {
+            result1.append(", ");
         }
+    }
+    System.out.println(result1.toString());
+
+    StringBuilder result2 = new StringBuilder();
+    for (int i = arr2.length - 1; i >= 0; i--) {
+        result2.append(arr2[i]);
+        if (i != 0) {
+            result2.append(", ");
+        }
+    }
+    System.out.println(result2.toString());
+
+    StringBuilder result3 = new StringBuilder();
+    for (int i = arr3.length - 1; i >= 0; i--) {
+        result3.append(arr3[i]);
+        if (i != 0) {
+            result3.append(", ");
+        }
+    }
+    System.out.println(result3.toString());
+
+    // task 4
+    System.out.println("Task 4");
+    int [] arr = {1, 2, 3};
+    for (int i = 0; i < arr.length; i++) {
+       if (arr[i] % 2 != 0) {
+           arr[i]++;
+        }
+    }
+    for (int num : arr) {
+        System.out.print(num + " ");
     }
 }
